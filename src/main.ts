9d491f285f17f6ca886b5ca1834fd52357dbed33
shellChang <1428667939@qq.com>
2019-07-04 20:57:40 +0800

    add static files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication, ExpressAdapter } from '@nestjs/platform-express'
 import { AppModule } from './app.module';
+import { join } from 'path'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app:NestExpressApplication  = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'), {prefix: '/static/'})
+  app.setBaseViewsDir(join(__dirname, '..', 'public/views')) // 放视图的文件
+  // app.setViewEngine({
+  //   engine: {
+  //    html:,
+  //   }
+  // });
   await app.listen(3000);
 }
 bootstrap();
